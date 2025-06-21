e05af202bd6ac265b0c04c366dfe93943ed854f1
duyhvt <thanhduy@Thanhs-MacBook-Pro.local>
2025-06-21 15:27:29 +0700

    Initial Commit

---
+++ src/general/HowToDebug/Main.java
@@ -0,0 +1,23 @@
+package general.HowToDebug;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        a++;
+//        int b = 0;
+//        int[] arr = {1, 2, 3};
+
+        for (int i = 0; i < 10; i++) {
+            a++;
+            System.out.println("a = " + a);
+            F(a);
+        }
+
+        System.out.println("DONE!");
+    }
+
+    private static void F(int a) {
+        a++;
+        System.out.println("a = " + a);
+    }
+}
