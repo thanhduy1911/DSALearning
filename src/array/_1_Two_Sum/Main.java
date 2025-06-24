7847ff5dc3ee7d40262db60038884a642007aa4b
duyhvt <thanhduy@Thanhs-MacBook-Pro.local>
2025-06-24 16:39:16 +0700

    ID: 1

---
+++ src/array/_1_Two_Sum/Main.java
@@ -0,0 +1,20 @@
+package array._1_Two_Sum;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums1 = {2,7,11,15};
+        int target1 = 9;
+        int[] nums2 = {2,3,4};
+        int target2 = 6;
+        int[] nums3 = {3,3};
+        int target3 = 6;
+
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(nums1, target1)));
+        System.out.println(Arrays.toString(solution.twoSum(nums2, target2)));
+        System.out.println(Arrays.toString(solution.twoSum(nums3, target3)));
+
+    }
+}
