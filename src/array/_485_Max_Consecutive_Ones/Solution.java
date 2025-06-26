80d48a4faa8c5c689d690cb9c49e68d38f655c33
duyhvt <thanhduy@Thanhs-MacBook-Pro.local>
2025-06-26 16:08:46 +0700

    ID: 485

---
+++ src/array/_485_Max_Consecutive_Ones/Solution.java
@@ -0,0 +1,21 @@
+package array._485_Max_Consecutive_Ones;
+
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (num == 0) {
+                count = 0;
+            } else {
+                count++;
+            }
+
+            if (max < count) {
+                max = count;
+            }
+        }
+        return max;
+    }
+}
