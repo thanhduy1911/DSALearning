61e13e56e7021dbd0627457cdb726ade48d27eeb
duyhvt <thanhduy@Thanhs-MacBook-Pro.local>
2025-06-28 09:08:28 +0700

    ID: 217

---
+++ src/array/_217_Contains_Duplicate/Main.java
@@ -0,0 +1,10 @@
+package array._217_Contains_Duplicate;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 1}));
+        System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 4}));
+        System.out.println(solution.containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+    }
+}
---
+++ src/array/_217_Contains_Duplicate/Solution.java
@@ -0,0 +1,22 @@
+package array._217_Contains_Duplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+
+        // create a set
+        Set<Integer> set = new HashSet<>();
+        // enhance for loop
+        for (int num : nums) {
+            if (set.contains(num)) return true; // is duplicate --> return true immediate
+            // if !duplicate --> add to HashSet set
+            set.add(num);
+        }
+
+        // if distinct --> nothing duplicate
+        return false;
+    }
+}
