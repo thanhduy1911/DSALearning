2735e71264d6247806ac2c78fa87422fb7d2518f
duyhvt <thanhduy@Thanhs-MacBook-Pro.local>
2025-07-02 13:45:07 +0700

    ID: 3330

---
+++ src/string/_3330_Find_The_Original_Typed_String_I/Main.java
@@ -0,0 +1,15 @@
+package string._3330_Find_The_Original_Typed_String_I;
+
+public class Main {
+    public static void main(String[] args) {
+        // test case
+        String word = "abbcccc";
+        String word1 = "abcd";
+        String word2 = "aaaa";
+
+        Solution solution = new Solution();
+        System.out.println(solution.possibleStringCount(word));
+        System.out.println(solution.possibleStringCount(word1));
+        System.out.println(solution.possibleStringCount(word2));
+    }
+}
